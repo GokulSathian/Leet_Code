944100240830edf3956385fd3bc5b4d09591a355
Gokul Sathian <pullarikalgokul@gmail.com>
2023-06-06 23:40:43 -0400

    first commit

---
+++ TwoSum_HashMap/twosum_hashmap.py
@@ -0,0 +1,7 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        x={}
+        for i,j in enumerate(nums):
+            if target-j in x:
+                return [i,x[target-j]]
+            x[j]=i
