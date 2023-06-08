042b10d3d0aea98f294be167043c3691e2faa5e6
Gokul Sathian <pullarikalgokul@gmail.com>
2023-06-07 23:14:59 -0400

    Day1&2

---
+++ Day#1TwoSum_hasmap.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        x={}
+        for i,j in enumerate(nums):
+            if target-j in x:
+                return [i,x[target-j]]
+            x[j]=i
+
+
