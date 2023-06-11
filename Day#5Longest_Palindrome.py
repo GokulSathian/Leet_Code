c7c56257db7dd8a0e6321a5262c27fad8163523e
Gokul Sathian <pullarikalgokul@gmail.com>
2023-06-11 16:20:52 -0400

    Day5

---
+++ Day#5Longest_Palindrome.py
@@ -0,0 +1,20 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        l,r=0,len(s)-1
+        collection=[]
+        count=0
+        if len(s)==1:
+            collection.append(s)
+
+
+        for l in range(len(s)-1):
+            front,end="",""
+            print(l,r)
+            while r>=l:
+                if s[l] ==s[r]:
+                    string1=s[l:r+1]
+                    if string1 == string1[::-1]:
+                        collection.append(string1)
+                r-=1              
+            r=len(s)-1
+        return max(collection,key=len)
